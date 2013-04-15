5fa01af59c710b156fdce692fbb9ce2f2a48fc1e
Mark Feeney <mark.feeney@iact.ie>
2013-04-15 08:11:32 +0100

    Assignment 3 Part 4 Final Version

---
+++ Comp41550 Project/NetworkScanner/NetworkScanner/host.h
@@ -0,0 +1,13 @@
+//
+//  host.h
+//  NetworkScanner
+//
+//  Created by Mark Feeney on 15/04/2013.
+//  Copyright (c) 2013 Mark Feeney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface host : NSObject
+
+@end
